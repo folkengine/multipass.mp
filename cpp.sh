7aef84b7b32bed1c2c7c79a73998c1889d5a4186
folkengine <gaoler@electronicpanopticon.com>
2022-12-30 10:07:12 -0800

    added cpp

---
+++ cpp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+multipass launch ubuntu --name gt --cpus 2 --mem 2G --disk 8G
+multipass exec gt -- bash -c 'sudo apt-get update && apt-get install -y apt-transport-https'
+multipass exec gt -- bash -c 'sudo apt install -y git python3 cmake g++ python-is-python3'
+multipass exec gt -- bash -c 'cd /home/ubuntu/'
+multipass exec gt -- bash -c 'git clone https://github.com/devplaybooks/cpp_gtest.git'
+multipass exec gt -- bash -c 'cd cpp_gtest/'
+multipass exec gt -- bash -c 'sh bin/doit'
+# Setup daemon.
+
+echo "Multipass gt instance created."
