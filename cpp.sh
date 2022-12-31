7e05981f23136dd71e3a64bba511391623b7105e
folkengine <gaoler@electronicpanopticon.com>
2022-12-30 19:24:23 -0800

    fixed doit

--- cpp.sh
+++ cpp.sh
@@ -4,8 +4,7 @@ multipass exec gt -- bash -c 'sudo apt-get update && apt-get install -y apt-tran
 multipass exec gt -- bash -c 'sudo apt install -y git python3 cmake g++ python-is-python3'
 multipass exec gt -- bash -c 'cd /home/ubuntu/'
 multipass exec gt -- bash -c 'git clone https://github.com/devplaybooks/cpp_gtest.git'
-multipass exec gt -- bash -c 'cd cpp_gtest/'
-multipass exec gt -- bash -c 'sh bin/doit'
+multipass exec gt -- bash -c 'cd /home/ubuntu/cpp_gtest && bin/doit'
 # Setup daemon.
 
 echo "Multipass gt instance created."
